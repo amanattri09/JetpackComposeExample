c35d23fa4346006bda628a00171d7a2ba30b45e6
attriaman2520 <attriaman2520@seasiainfotech.com>
2022-12-27 18:30:40 +0530

    -added toolbar

--- app/src/main/java/com/example/composeexamplev3/common/presentation/ui/post/PostActivity.kt
+++ app/src/main/java/com/example/composeexamplev3/common/presentation/ui/post/PostActivity.kt
@@ -1,15 +1,16 @@
 package com.example.composeexamplev3.common.presentation.ui.post
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -66,14 +67,15 @@ fun postApp(mutableLiveDataPosts: MutableState<PostResponse?>) {
         composable(route = HomeScreen.ROUTE) {
             postListScreen(mutableLiveDataPosts, navController)
         }
-        composable(route = PostDetailScreen.ROUTE + "/{${PostDetailScreen.ARGUMENTS.POST_TITLE}}/{${PostDetailScreen.ARGUMENTS.POST_DISCRIPTION}}", arguments = listOf(
-            navArgument(name = PostDetailScreen.ARGUMENTS.POST_TITLE) {
-                type = NavType.StringType
-            },
-            navArgument(name = PostDetailScreen.ARGUMENTS.POST_DISCRIPTION) {
-                type = NavType.StringType
-            }
-        )) {
+        composable(route = PostDetailScreen.ROUTE + "/{${PostDetailScreen.ARGUMENTS.POST_TITLE}}/{${PostDetailScreen.ARGUMENTS.POST_DISCRIPTION}}",
+            arguments = listOf(
+                navArgument(name = PostDetailScreen.ARGUMENTS.POST_TITLE) {
+                    type = NavType.StringType
+                },
+                navArgument(name = PostDetailScreen.ARGUMENTS.POST_DISCRIPTION) {
+                    type = NavType.StringType
+                }
+            )) {
             PostDetailScreen(
                 navController,
                 it.arguments?.getString(PostDetailScreen.ARGUMENTS.POST_TITLE),
@@ -83,32 +85,54 @@ fun postApp(mutableLiveDataPosts: MutableState<PostResponse?>) {
     }
 }
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun postListScreen(
     mutableLiveDataPosts: MutableState<PostResponse?>,
     navController: NavHostController
 ) {
-    LazyColumn(content = {
-        mutableLiveDataPosts.value?.forEach {
-            item {
-                Log.i(PostActivity::class.java.name, "inside item")
-                Text(text = it.title, modifier = Modifier
-                    .fillMaxWidth()
-                    .height(40.dp)
-                    .padding(horizontal = 16.dp)
-                    .clickable {
-                        navController.navigate(PostDetailScreen.ROUTE + "/${it.title + "/" + it.body}")
-                    })
+    Scaffold(topBar = {
+        TopAppBar(
+            title = { Text(text = "Home") }
+        )
+    }, content = {
+        LazyColumn(content = {
+            mutableLiveDataPosts.value?.forEach {
+                item {
+                    Log.i(PostActivity::class.java.name, "inside item")
+                    Text(text = it.title, modifier = Modifier
+                        .fillMaxWidth()
+                        .height(40.dp)
+                        .padding(horizontal = 16.dp)
+                        .clickable {
+                            navController.navigate(PostDetailScreen.ROUTE + "/${it.title + "/" + it.body}")
+                        })
+                }
             }
-        }
+        })
     })
 }
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun PostDetailScreen(postResponseItem: NavHostController, title: String?, description: String?) {
-    title?.let { title ->
-        description?.let { description ->
-            Text(text = "title = $title + Description = $description")
+    Scaffold(topBar = {
+        TopAppBar(
+            title = { Text(text = "Post Detail") }
+        )
+    }
+    ) {
+        Column(
+            modifier = Modifier
+                .wrapContentHeight()
+                .padding(16.dp)
+        ) {
+            title?.let { title ->
+                Text(text = "Title : $title", modifier = Modifier.fillMaxWidth())
+            }
+            description?.let { description ->
+                Text(text = "Description : $description", modifier = Modifier.fillMaxWidth())
+            }
         }
     }
 }
